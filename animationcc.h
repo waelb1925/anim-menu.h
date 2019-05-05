5d118e02501f2b784239192179ac1d3b0eed4f08
waelb1925 <47981286+waelb1925@users.noreply.github.com>
2019-05-05 08:35:29 -0700

    Add files via upload

---
+++ animationcc.h
@@ -0,0 +1,11 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+#include <SDL/SDL.h>
+
+
+void animation(SDL_Surface *ecran);
+void animation2(SDL_Surface *ecran);
+void load(SDL_Surface *ecran);
+void choisir_perso(SDL_Surface *ecran);
+
+#endif
